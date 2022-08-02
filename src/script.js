6917f6bb45253ca58574add715ba087597d4de7e
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-08-03 00:45:17 +0500

    push

--- src/script.js
+++ src/script.js
@@ -1,1 +1,21 @@
-addEventListener
+const imgSlider = (event) => {
+let img = document.querySelector('#img').innerHTML; //"./img/img01.jpg";
+	// console.log(img)
+	// console.log(typeof(img))
+
+	// console.log(event);
+
+	const Xaxis= event.clientX;
+	console.log(event.clientX);
+	// console.log(event.PointerEvent.clientX);
+	// console.log(event.clientY);
+
+
+
+}
+
+let btn;
+
+btn.addEventListener('click',()=>{
+
+});
